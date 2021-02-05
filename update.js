c1981fe300d9c3f9578e802d62adb987423e0331
Sebastian Krüger <2pi_r2@gmx.de>
2021-02-05 17:48:45 +0100

    fix

--- update.js
+++ update.js
@@ -3,13 +3,30 @@ const fs = require('fs')
 const pack = JSON.parse(fs.readFileSync('./Leaflet/package.json', 'utf8'))
 const version = fs.readFileSync('./.commithash', 'utf8')
 
-pack.version = pack.version + '-' + version.trim()
+pack.version = pack.version + '-' + version.trim().slice(0, 9)
 pack.name = '@ec-nordbund/leaflet'
 
 delete pack.files
 
-const file = `export * from '../src/Leaflet.js'`
+const file = `
+import './fix.js'
+export * from '../src/Leaflet.js'`
 
+const fix = `
+if ((process && !process.browser) || !globalThis.window) {
+  globalThis.window = {
+    devicePixelRatio: 1,
+    addEventListener: () => {}
+  }
+  globalThis.navigator = {
+    userAgent: '',
+    platform: ''
+  }
+  globalThis.document = { documentElement: { style: {} }, createElement: () => ({ getContext: () => { } }) }
+}
+`
+
+fs.writeFileSync('./Leaflet/dist/fix.js', fix)
 fs.writeFileSync('./Leaflet/dist/leaflet-src.js', file)
 fs.writeFileSync('./Leaflet/package.json', JSON.stringify(pack, null, 2))
 
